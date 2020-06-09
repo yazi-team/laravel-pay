ff77caa74d328866e9a4a1fd61daa742511e3975
小凡 <ba9699752@gmail.com>
2020-06-10 00:12:25 +0800

    对接多多支付

---
+++ src/Gateways/Ddpay.php
@@ -0,0 +1,283 @@
+<?php
+
+namespace Xiaofan\Pay\Gateways;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Xiaofan\Pay\Contracts\GatewayApplicationInterface;
+use Xiaofan\Pay\Contracts\GatewayInterface;
+use Xiaofan\Pay\Events;
+use Xiaofan\Pay\Exceptions\GatewayException;
+use Xiaofan\Pay\Exceptions\InvalidArgumentException;
+use Xiaofan\Pay\Exceptions\InvalidConfigException;
+use Xiaofan\Pay\Exceptions\InvalidGatewayException;
+use Xiaofan\Pay\Exceptions\InvalidSignException;
+use Xiaofan\Pay\Gateways\Ddpay\Support;
+use Yansongda\Supports\Collection;
+use Yansongda\Supports\Config;
+use Yansongda\Supports\Str;
+use Xiaofan\Pay\Contracts\Payable;
+use Xiaofan\Pay\Entity\PurchaseResult;
+
+/**
+ * @method Response   app(array $config)      APP 支付
+ * @method Collection pos(array $config)      刷卡支付
+ * @method Collection scan(array $config)     扫码支付
+ * @method Collection transfer(array $config) 帐户转账
+ * @method Response   wap(array $config)      手机网站支付
+ * @method Response   web(array $config)      电脑支付
+ * @method Collection mini(array $config)     小程序支付
+ */
+class Ddpay implements GatewayApplicationInterface {
+
+    /**
+     * Const mode_normal.
+     */
+    const MODE_NORMAL = 'normal';
+
+    /**
+     * Const mode_dev.
+     */
+    const MODE_DEV = 'dev';
+    
+    /**
+     * Const mode_query.
+     */
+    const MODE_QUERY = 'query';
+
+    /**
+     * Const mode_service.
+     */
+    const MODE_SERVICE = 'service';
+
+    /**
+     * Const url.
+     */
+    const URL = [
+        self::MODE_NORMAL => 'http://www.ddpay2.com:3020/api/pay/create_order',
+        self::MODE_DEV => 'http://www.ddpay2.com:3020/api/pay/create_order',
+        self::MODE_QUERY => 'http://www.ddpay2.com:3020/api/pay/query_order',
+    ];
+
+    /**
+     * Ddpay payload.
+     *
+     * @var array
+     */
+    protected $payload;
+
+    /**
+     * Ddpay gateway.
+     *
+     * @var string
+     */
+    protected $gateway;
+
+    /**
+     * extends.
+     *
+     * @var array
+     */
+    protected $extends;
+
+    /**
+     * Bootstrap.
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @throws \Exception
+     */
+    public function __construct(Config $config) {
+        $this->gateway = Support::create($config)->getBaseUri();
+        $this->payload = [
+            'mchId' => $config->get('mch_id'),
+            'appId' => $config->get('app_id'),
+            'productId' => $config->get('pay_type'),
+            'returnUrl' => $config->get('return_url'),
+            'notifyUrl' => $config->get('notify_url'),
+        ];
+    }
+
+    /**
+     * Magic pay.
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @param string $method
+     * @param array  $charge
+     *
+     * @throws GatewayException
+     * @throws InvalidArgumentException
+     * @throws InvalidConfigException
+     * @throws InvalidGatewayException
+     * @throws InvalidSignException
+     *
+     * @return Response|Collection
+     */
+    public function __call($method, Payable $charge) {
+        return $this->pay($method, $charge);
+    }
+
+    /**
+     * Pay an order.
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @param string $gateway
+     * @param array  $charge
+     *
+     * @throws InvalidGatewayException
+     *
+     * @return Response|Collection
+     */
+    public function pay($gateway, Payable $charge) {
+//        Events::dispatch(new Events\PayStarting('Ddpay', $gateway, $params));
+        $request = app('request');
+        $request->setTrustedProxies($request->getClientIps(), Request::HEADER_X_FORWARDED_ALL);
+        $this->payload['currency'] = 'cny';
+        $this->payload['mchOrderNo'] = $charge->getTradeNo();
+        $this->payload['amount'] = intval($charge->getAmount());
+        $this->payload['clientIp'] = $request->getClientIp();
+        $this->payload['subject'] = $charge->getSubject();
+        $this->payload['body'] = $charge->getBody();
+        $this->payload['extra'] = $charge->getUser();
+        $this->payload['sign'] = Support::generateSign($this->payload);
+//        dump($this->payload);die;
+        $gateway = get_class($this) . '\\' . Str::studly($gateway) . 'Gateway';
+
+        if (class_exists($gateway)) {
+            return $this->makePay($gateway);
+        }
+
+        throw new InvalidGatewayException("Pay Gateway [{$gateway}] not exists");
+    }
+
+    /**
+     * Verify sign.
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @param array|null $data
+     *
+     * @throws InvalidSignException
+     * @throws InvalidConfigException
+     */
+    public function verify($data = null, bool $refund = false): PurchaseResult {
+        if (is_null($data)) {
+            $request = Request::createFromGlobals();
+
+            $data = $request->request->count() > 0 ? $request->request->all() : $request->query->all();
+        }
+
+        Events::dispatch(new Events\RequestReceived('Ddpay', '', $data));
+
+        if (Support::verifySign($data)) {
+            $find = $this->find($data['mchOrderNo']);
+            return new PurchaseResult('ddpay', 
+                    $data['mchOrderNo'], 
+                    $data['payOrderId'], 
+                    intval($data['amount']), 
+                    $data['status'] == 2 && $find['status'] == 2, 
+                    date("Y-m-d H:i:s"), $data);
+        }
+
+        Events::dispatch(new Events\SignFailed('Ddpay', '', $data));
+
+        throw new InvalidSignException('Ddpay Sign Verify FAILED', $data);
+    }
+
+    /**
+     * Query an order.
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @param string $order
+     *
+     * @throws GatewayException
+     * @throws InvalidConfigException
+     * @throws InvalidSignException
+     */
+    public function find($order, string $type = 'wap'): Collection {
+        $this->payload['mchOrderNo'] = $order;
+        $this->payload['sign'] = Support::generateSign($this->payload);
+        
+        Events::dispatch(new Events\MethodCalled('Ddpay', 'Find', $this->gateway, $this->payload));
+
+        return Support::requestApi($this->payload);
+    }
+
+    /**
+     * Refund an order.
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @throws GatewayException
+     * @throws InvalidConfigException
+     * @throws InvalidSignException
+     */
+    public function refund(array $order): Collection {
+        
+    }
+
+    /**
+     * Cancel an order.
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @param array|string $order
+     *
+     * @throws GatewayException
+     * @throws InvalidConfigException
+     * @throws InvalidSignException
+     */
+    public function cancel($order): Collection {
+    }
+
+    /**
+     * Close an order.
+     *
+     * @param string|array $order
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @throws GatewayException
+     * @throws InvalidConfigException
+     * @throws InvalidSignException
+     */
+    public function close($order): Collection {
+        
+    }
+
+    /**
+     * Reply success to alipay.
+     *
+     * @author yansongda <me@yansongda.cn>
+     */
+    public function success(): Response {
+        Events::dispatch(new Events\MethodCalled('Ddpay', 'Success', $this->gateway));
+
+        return Response::create('SUCCESS');
+    }
+
+    /**
+     * Make pay gateway.
+     *
+     * @author yansongda <me@yansongda.cn>
+     *
+     * @throws InvalidGatewayException
+     *
+     * @return Response|Collection
+     */
+    protected function makePay(string $gateway) {
+        $app = new $gateway();
+
+        if ($app instanceof GatewayInterface) {
+            return $app->pay($this->gateway, array_filter($this->payload, function ($value) {
+                                return '' !== $value && !is_null($value);
+                            }));
+        }
+
+        throw new InvalidGatewayException("Pay Gateway [{$gateway}] Must Be An Instance Of GatewayInterface");
+    }
+
+}
